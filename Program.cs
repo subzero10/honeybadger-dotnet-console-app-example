288fee9a88710809110070808ee9f6ba9a0d8df7
Pangratios Cosma <pagcosma@gmail.com>
2025-02-15 10:25:47 +0200

    feat: console app example project

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Honeybadger;
+using Honeybadger.DotNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.AddHoneybadger();
+
+builder.Logging.AddHoneybadger();
+using var host = builder.Build();
+
+var source = new CancellationTokenSource();
+var token = source.Token;
+
+// switch  between "manual" and "logger" to test the different ways to notify Honeybadger
+// "automatic" is not supported yet in console apps
+const string flag = "logger";
+switch (flag)
+{
+    case "manual":
+        await NotifyHoneybadgerManually(host.Services);
+        await source.CancelAsync();
+        break;
+    // case "automatic":
+        // todo: Unhandled exceptions are only caught in .Net Core web apps (using a middleware).
+        //       In a console app, we need to catch and report the exceptions manually.
+        // NotifyHoneybadgerAutomatically();
+        // break;
+    case "logger":
+        NotifyHoneybadgerWithLogger(host.Services);
+        await source.CancelAsync();
+        break;
+    default:
+        throw new ArgumentOutOfRangeException();
+}
+
+
+await host.RunAsync(token);
+
+return;
+
+static async Task NotifyHoneybadgerManually(IServiceProvider serviceProvider)
+{
+    var honeybadger = serviceProvider.GetRequiredService<IHoneybadgerClient>();
+    await honeybadger.NotifyAsync(new Exception("Something went wrong"));
+}
+
+static void NotifyHoneybadgerAutomatically()
+{
+    throw new Exception("Something went wrong");
+}
+
+static void NotifyHoneybadgerWithLogger(IServiceProvider serviceProvider)
+{
+    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+    logger.LogError("Something went wrong. This was reported using a logger instance.");
+}
+
---
+++ README.md
@@ -0,0 +1,17 @@
+# Honeybadger .Net Example
+
+This is an example of how to use the Honeybadger .Net library to report
+exceptions to Honeybadger.
+
+## Usage
+
+1. Clone this repository.
+2. Open the solution in Visual Studio.
+3. Replace the `Honeybadger.ApiKey` in `appsettings.json` with your Honeybadger API key.
+4. In `Program.cs`, replace the `flag` variable with one of the values to test the different ways of reporting exceptions.
+5. Run the application.
+
+## Next Steps
+
+For more information, such as configuration and more advanced usage, checkout
+the [GitHub repository for the Honeybadger .Net package](https://github.com/honeybadger-io/honeybadger-dotnet).
